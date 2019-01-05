5dd2eb454eb02e67e27fa3f0e6212888172579fc
aguilaraul <raula@live.com>
2019-01-05 01:28:48 -0800

    Initial commit

---
+++ Python/Warmup-1/sum_double.py
@@ -0,0 +1,14 @@
+def sum_double(a, b):
+    if a == b:
+        return (a+b)*2
+    else:
+        return a+b
+
+print(sum_double(1,2)) # 3
+print(sum_double(3,2)) # 5
+print(sum_double(2,2)) # 8
+print(sum_double(-1,0)) # -1
+print(sum_double(3,3)) # 12
+print(sum_double(0,0)) # 0
+print(sum_double(0,1)) # 1
+print(sum_double(3,4)) # 7
