fe3d5c9f3e230a6299fd27f57bf5a68e35a4a8f0
aguilaraul <raula@live.com>
2019-01-13 14:57:28 -0800

    Java:Warmup-2

--- Java/Warmup-2/ArrayFront9.java
+++ Java/Warmup-2/ArrayFront9.java
@@ -33,17 +33,17 @@ public class ArrayFront9 {
 
 	public static boolean arrayFront9(int[] nums) {
 		if(nums.length < 4) {
-    		for(int i = 0; i < nums.length; i++) {
-      			if(nums[i] == 9) {
-        			return true;
-      			}
-    		}
+    			for(int i = 0; i < nums.length; i++) {
+      				if(nums[i] == 9) {
+        				return true;
+      				}
+    			}
   		} else {
-    		for(int i = 0; i < 4; i++) {
-      			if(nums[i] == 9) {
-        			return true;
-      			}
-    		}
+    			for(int i = 0; i < 4; i++) {
+      				if(nums[i] == 9) {
+        				return true;
+      				}
+    			}
   		}
   		return false;
 	}
