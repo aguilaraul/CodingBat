5111b79a17b1ec8e77bea7d9d3e77c5672537743
aguilaraul <raula@live.com>
2019-01-14 12:18:15 -0800

    Java:AP-1

--- Java/AP-1/Scores100.java
+++ Java/AP-1/Scores100.java
@@ -7,17 +7,25 @@ public class Scores100 {
 		int[] scores1 = {1, 100, 100}; //true
 		int[] scores2 = {1, 100, 99, 100}; //false
 		int[] scores3 = {100, 1, 100, 100}; //true
+		int[] scores4 = {100, 1, 100, 1}; //false
+		int[] scores5 = {1, 2,3,4,5}; //false
+		int[] scores6 = {1,2,100,4,5}; //false
+		
 
 		System.out.println(scores100(scores1));
 		System.out.println(scores100(scores2));
 		System.out.println(scores100(scores3));
+		System.out.println(scores100(scores4));
+		System.out.println(scores100(scores5));
+		System.out.println(scores100(scores6));
 	}
 
 	public static boolean scores100(int[] scores) {
 		int temp = 0;
-		for(int i = 0; i < scores.length; i++) {
-			if(scores[i] == 100) {
-				temp = sc
+		for(int i = 0; i < scores.length-1; i++) {
+			if(scores[i] == 100 && scores[i+1] == 100) return true;
+		}
+		return false;
 	}
 }
 
---
+++ Java/AP-1/ScoresAverage.java
@@ -0,0 +1,54 @@
+/*
+ * Given an array of scores, compute the int average of the first half and
+ * the second half, and return whichever is larger. We'll say that the second
+ * half begins at index length/2. The array length will be at least 2.
+ */
+public class ScoresAverage {
+	public static void main(String[] args) {
+		int[] scores1 = {2,2,4,4}; // 4
+		int[] scores2 = {4,4,4,2,2,2}; // 4
+		int[] scores3 = {3,4,5,1,2,3}; // 4
+		int[] scores4 = {5,6}; // 6
+		int[] scores5 = {5,4}; // 5
+		int[] scores6 = {5,4,5,6,2,1,2,3}; // 5
+
+		System.out.println(scoresAverage(scores1));
+		System.out.println(scoresAverage(scores2));
+		System.out.println(scoresAverage(scores3));
+		System.out.println(scoresAverage(scores4));
+		System.out.println(scoresAverage(scores5));
+		System.out.println(scoresAverage(scores6));
+	}
+
+	public static int scoresAverage(int[] scores) {
+		int[] firstHalf = new int[scores.length/2];
+		int j = 0;
+		for(int i = 0; i < scores.length/2; i++) {
+			firstHalf[j] = scores[i];
+			j++;
+		}
+		int[] secondHalf = new int[scores.length/2];
+		j = 0;
+		for(int i = scores.length/2; i < scores.length; i++) {
+			secondHalf[j] = scores[i];
+			j++;
+		}
+
+		int larger = average(firstHalf);
+		if(average(secondHalf) > larger) {
+			larger = average(secondHalf);
+		}
+		return larger;
+	}
+
+	public static int average(int[] scores) {
+		int length = scores.length;
+		int sum = 0;
+
+		for(int number : scores) {
+			sum += number;
+		}
+
+		return sum/length;
+	}
+}
