f5707c0dd359c7c9d67e1bd806cc29e223367707
Raul Aguilar <raula@live.com>
2019-01-26 22:27:49 -0800

    Java:String-2

---
+++ Java/String-2/DoubleChar.java
@@ -0,0 +1,15 @@
+/*
+ * Given a string, return a string where for every char in the original,
+ * there are two chars.
+ */
+public class DoubleChar {
+	public String doubleChar(String str) {
+		String result = "";
+
+		for(int i = 0; i < str.length(); i++) {
+			result = result + str.charAt(i) + str.charAt(i);
+		}
+
+		return result;
+	}
+}
